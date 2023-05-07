92848b37e1fbde45b69f1e7db16e47b76fca1ec0
Marcelo Santos <rocha.mer21@gmail.com>
2023-05-07 13:47:59 -0300

    feat: create Screen constant Navigation

--- app/src/main/java/com/developermarcelo/diarymyapp/util/Constants.kt
+++ app/src/main/java/com/developermarcelo/diarymyapp/util/Constants.kt
@@ -3,4 +3,5 @@ package com.developermarcelo.diarymyapp.util
 object Constants {
     const val APP_ID = "diaryapp-xafbd"
     const val CLIENT_ID = "418870754588-auqvq3t27eih8h7ut5p11erm660hbhn8.apps.googleusercontent.com"
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
 }
